82bca3d829fd94a4cbb9aaff12ffa2d8d7bd60bc
hershi <hershi@gmail.com>
2016-04-20 23:40:36 -0700

    Solved 'leap'

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn is_leap_year(year: i32) -> bool {
+    if year % 400 == 0
+    {
+        return true;
+    }
+    
+    if year % 100 == 0
+    {
+        return false;
+    }
+
+    year % 4 == 0
+}
+
