55cf82aa408f7a235b15e3ff735cc242215f1dda
hershi <hershi@gmail.com>
2016-06-05 13:15:34 -0700

    Minor formatting change

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -5,7 +5,8 @@ use regex::Regex;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
     let word_splitter = Regex::new(r"[^a-z0-9]+").unwrap();
-    word_splitter.split(&s.to_lowercase()).filter(|word| !word.is_empty()).fold(HashMap::new(), 
+    word_splitter.split(&s.to_lowercase()).filter(|word| !word.is_empty()).fold(
+        HashMap::new(), 
         |mut word_map, word| { 
             *(word_map.entry(String::from(word)).or_insert(0)) += 1;
             word_map
