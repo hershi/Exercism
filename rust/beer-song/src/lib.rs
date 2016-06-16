7a38935c23fe9ae08046d5dd50ff59f8092f9976
hershi <hershi@gmail.com>
2016-06-16 13:44:47 -0700

    Beer song: More cleanup - inline small functions

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -6,24 +6,22 @@ fn bottles_of_beer(count : u32) -> String {
     format!("{} {} of beer", how_many, bottles)    
 }
 
-fn get_next_count(count : u32) -> u32 {
-    if count > 0 { count - 1 } else { COUNT_UPPER_BOUND - 1 }     
-}
-
 fn get_action(ordinal : u32) -> String {
-    if ordinal == 0 {"Go to the store and buy some more".to_string()} 
-    else {format!("Take {} down and pass it around", 
-                  if ordinal == 1 {"it"} else {"one"})}.to_string()
+    if ordinal == 0 { return "Go to the store and buy some more".to_string()}; 
+
+    let word = if ordinal == 1 {"it"} else {"one"};
+    format!("Take {} down and pass it around", word).to_string()
 }
 
 pub fn verse(ordinal : u32) -> String {
     let bottles = bottles_of_beer(ordinal);
+    let next_count = if ordinal > 0 { ordinal - 1 } else { COUNT_UPPER_BOUND - 1 };     
 
     format!("{} on the wall, {}.\n\
     {action}, {} on the wall.\n",
         bottles,
         bottles.to_lowercase(),
-        bottles_of_beer(get_next_count(ordinal)).to_lowercase(),
+        bottles_of_beer(next_count).to_lowercase(),
         action = get_action(ordinal))
 }
 
