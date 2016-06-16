8a7a3fb1deb26c602ebb52c09235628b5e24ed28
hershi <hershi@gmail.com>
2016-06-16 13:41:37 -0700

    Bottles of Beer v1: Cleaned up code

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,11 +1,9 @@
 const COUNT_UPPER_BOUND : u32 = 100;
 
-fn how_many(count : u32) -> String {
-    if count > 0 {count.to_string()} else {"No more".to_string()}
-}
-
-fn bottle_or_bottles(count : u32) -> String {
-    if count == 1 { "bottle".to_string() } else { "bottles".to_string() }
+fn bottles_of_beer(count : u32) -> String {
+    let how_many = if count > 0 {count.to_string()} else {"No more".to_string()}; 
+    let bottles = if count == 1 { "bottle" } else { "bottles" };
+    format!("{} {} of beer", how_many, bottles)    
 }
 
 fn get_next_count(count : u32) -> u32 {
@@ -18,32 +16,21 @@ fn get_action(ordinal : u32) -> String {
                   if ordinal == 1 {"it"} else {"one"})}.to_string()
 }
 
-fn verse_internal(ordinal : u32) -> String {
-    let next_ordinal = get_next_count(ordinal);
-    let count = how_many(ordinal);
-
-
-    format!("{} {bottles} of beer on the wall, {} {bottles} of beer.\n\
-    {action}, {next_count} {next_bottles} of beer on the wall.\n\n",
-        count,
-        count.to_lowercase(),
-        bottles = bottle_or_bottles(ordinal),  
-        action = get_action(ordinal),    
-        next_count = how_many(next_ordinal).to_lowercase(),
-        next_bottles = bottle_or_bottles(next_ordinal)
-    )
-}
-
 pub fn verse(ordinal : u32) -> String {
-    let mut res = verse_internal(ordinal);
-    res.pop();
-    res
+    let bottles = bottles_of_beer(ordinal);
+
+    format!("{} on the wall, {}.\n\
+    {action}, {} on the wall.\n",
+        bottles,
+        bottles.to_lowercase(),
+        bottles_of_beer(get_next_count(ordinal)).to_lowercase(),
+        action = get_action(ordinal))
 }
 
 pub fn sing(start : u32, end : u32) -> String {
-    let mut res = (0..start - end + 1).fold(
-        String::new(), 
-        |mut acc, i| {acc.push_str(&verse_internal(start - i)); acc});
-    res.pop();
-    res
+    (end..start + 1).rev()
+                    .map(|ordinal| verse(ordinal))
+                    .collect::<Vec<String>>()
+                    .as_slice()
+                    .join("\n")
 }
