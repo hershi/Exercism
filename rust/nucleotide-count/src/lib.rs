5814251d66929f65aabc770973ef7814e395953c
hershi <hershi@gmail.com>
2016-06-18 21:13:09 -0700

    nucleotide count v1: use iteartors - map + collect

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -5,10 +5,5 @@ pub fn count(nucleotide : char, dna : &str) -> usize {
 }
 
 pub fn nucleotide_counts(dna : &str) -> HashMap<char, usize> {
-    let mut result = HashMap::new();
-    for nucleotide in ['A','C','T','G'].iter() {
-        result.insert(*nucleotide, count(*nucleotide, dna));
-    }
-
-    result    
+    "ACTG".chars().map(|n| (n, count(n, dna))).collect()
 }
