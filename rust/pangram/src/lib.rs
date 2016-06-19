230a5b994ce86ed3010322db3fd49b248bacc4cb
hershi <hershi@gmail.com>
2016-06-18 20:48:11 -0700

    Pangram: Char-by-char lowercasing - weird failure

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,9 +1,8 @@
 use std::collections::HashSet;
 
 pub fn is_pangram(sentence : &str) -> bool {
-    sentence.to_lowercase()
-            .chars()
-            .filter(|c| ('a' <= *c) && (*c <= 'z'))
+    sentence.chars()
+            .filter(|c| {let lc = c.to_lowercase().next().unwrap(); ('a' <= lc) && (lc <= 'z')})
             .collect::<HashSet<char>>()
             .len() == 26
 }
