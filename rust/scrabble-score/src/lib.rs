00147bc7fcb83021cb81695844784e54e2a77bb9
hershi <hershi@gmail.com>
2016-06-18 20:00:45 -0700

    Scrabble score v1: Solution with pattern matching

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -1,21 +1,16 @@
-use std::collections::HashMap;
-
-
-fn get_scoring_table() -> HashMap<char, u32> {
-    let mut scoring_table : HashMap<char, u32> = HashMap::new();
-    
-    for c in vec!['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'] { scoring_table.insert(c,1); }
-    for c in vec!['D', 'G'] { scoring_table.insert(c,2); }
-    for c in vec!['B', 'C', 'M', 'P'] { scoring_table.insert(c,3); }
-    for c in vec!['F', 'H', 'V', 'W', 'Y'] { scoring_table.insert(c,4); }
-    for c in vec!['K'] { scoring_table.insert(c,5); }
-    for c in vec!['J', 'X'] { scoring_table.insert(c,8); }
-    for c in vec!['Q', 'Z'] { scoring_table.insert(c,10); }
-    
-    scoring_table
+fn get_char_score(c : &char) -> u32 {
+    match *c {
+        'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+        'D' | 'G' => 2,
+        'B' | 'C' | 'M' | 'P' => 3,
+        'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+        'K' => 5,
+        'J' | 'X' => 8,
+        'Q' | 'Z' => 10,
+        _ => 0
+    }
 }
 
 pub fn score(word : &str) -> u32 {
-    let scoring_table = get_scoring_table();
-    word.to_uppercase().chars().fold(0, |score, c| score + scoring_table.get(&c).map_or(0, |x| *x))
+    word.to_uppercase().chars().fold(0, |score, c| score + get_char_score(&c))
 }
