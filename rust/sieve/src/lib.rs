3d8ddce3fece8706436856346c50532281f2403b
hershi <hershi@gmail.com>
2016-06-24 23:18:17 -0700

    Sieve v1: Iterate over indexes (no separate result)

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn primes_up_to(num : u32) -> Vec<u32> {
     let mut input = (2..num+1).collect::<Vec<u32>>();
-    let mut result = Vec::new();
+    let mut current_index = 0;
 
-    while !input.is_empty() {
-        let current = input[0];
-        result.push(current);
-        input.retain(|&x| x % current != 0);
+    while current_index < input.len() {
+        let current = input[current_index]; 
+        input.retain(|&x| x == current || x % current != 0);
+        current_index += 1;
     }
 
-    result
+    input
 }
