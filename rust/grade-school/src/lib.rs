ea8e19a633360b98e1af74c643c162f479143d92
hershi <hershi@gmail.com>
2016-07-11 00:09:31 -0700

    grade school v1: Sorted insertion

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -11,8 +11,16 @@ impl School {
 
     pub fn add(&mut self, grade: u32, student_name : &str) {
         let entry = self.grades.entry(grade).or_insert(Vec::new());
+
+        // To avoid running a full sort every time, we do a sorted insertion.
+        // Calling binary_search_by will return the correct insertion index,
+        // both in the case when a student with the given name is already in the
+        // class, and in the case when such a student is not in the class.
+        // We use binary_search_by to allow us to convert the String references
+        // to &str for comparison, because there's no 'cmp' defined in the standard
+        // library where the LHS is a &String and RHS is a &str.
         let insertion_index = 
-            match (*entry).binary_search(&(student_name.to_string())) {
+            match (*entry).binary_search_by(|s| (s.as_str()).cmp(student_name)) {
                 Ok(x) => x,
                 Err(x) => x
             };
@@ -22,6 +30,9 @@ impl School {
 
     pub fn grades(&self) -> Vec<u32> {
         let mut result = self.grades.keys().map(|k| *k).collect::<Vec<u32>>();
+
+        // Since keys() returns the keys in arbitrary order, we need to sort them
+        // before returning them.
         result.sort();
         result
     }
