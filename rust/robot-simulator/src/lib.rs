84811940915958933fe021f3563437e52a70fc2a
hershi <hershi@gmail.com>
2016-07-13 21:22:38 -0700

    Robot simulator: Better naming

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -10,47 +10,47 @@ pub enum Direction {
 }
 
 pub struct Robot {
-    facing : Direction,
-    location : (isize, isize),
+    direction : Direction,
+    position : (isize, isize),
 }
 
 impl Robot {
     pub fn new(x: isize, y: isize, d: Direction) -> Self {
-        Robot{facing : d, location : (x,y)}
+        Robot{direction : d, position : (x,y)}
     }
 
     pub fn turn_right(self) -> Self {
-        let new_direction = match self.facing {
+        let new_direction = match self.direction {
             Direction::North => Direction::East,
             Direction::East => Direction::South,
             Direction::South => Direction::West,
             Direction::West => Direction::North
         };
         
-        Robot{facing : new_direction, location : self.location}
+        Robot{direction : new_direction, position : self.position}
     }
 
     pub fn turn_left(self) -> Self {
-        let new_direction = match self.facing {
+        let new_direction = match self.direction {
             Direction::East => Direction::North,
             Direction::South => Direction::East,
             Direction::West => Direction::South,
             Direction::North => Direction::West 
         };
 
-        Robot{facing : new_direction, location : self.location}
+        Robot{direction : new_direction, position : self.position}
     }
 
     pub fn advance(self) -> Self {
-        let (x,y) = match self.facing {
+        let (x,y) = match self.direction {
             Direction::East => (1,0),
             Direction::South => (0,-1),
             Direction::West => (-1,0),
             Direction::North => (0,1) 
         };
 
-        let new_location = (self.location.0 + x, self.location.1 + y);
-        Robot{facing : self.facing, location : new_location}
+        let (a,b) = self.position;
+        Robot{direction : self.direction, position : (x + a, y + b)}
     }
 
     pub fn instructions(self, instructions: &str) -> Self {
@@ -65,10 +65,10 @@ impl Robot {
     }
 
     pub fn position(&self) -> (isize, isize) {
-        self.location
+        self.position
     }
 
     pub fn direction(&self) -> &Direction {
-        &self.facing
+        &self.direction
     }
 }
