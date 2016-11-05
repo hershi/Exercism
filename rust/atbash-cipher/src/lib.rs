df01ac85ada0ebd107b953c6b721c76dc67938ce
hershi <hershi@gmail.com>
2016-11-04 21:43:11 -0700

    Atbash-cipher v1: Arithmetic encoder

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,47 +1,76 @@
 use std::collections::HashMap;
 use std::ascii::AsciiExt;
+use std::iter::*;
 
-pub fn encode(phrase : &str) -> String {
-    let mapping = get_mapping();
-    let mut i = 0;
+trait Encoder {
+    fn encode(&self, c : &char) -> char;
+}
+
+fn basic_encode(phrase : &str, encoder : &Encoder) -> Vec<char> {
     phrase
         .to_lowercase()
         .chars()
-        .map(|x| mapping.get(&x).map_or(x, |y| *y) )
         .filter(|c| c.is_alphanumeric() && c.is_ascii())
-        .fold(String::new(), |mut acc, c| {
-            if i != 0 && i % 5 == 0 { acc.push(' ') };
-            i += 1;
+        .map(|x| encoder.encode(&x) )
+        .collect()
+}
+
+pub fn encode(phrase : &str) -> String {
+    let encoder = ArithmeticEncoder{};
+    basic_encode(phrase, &encoder)
+        .into_iter()
+        .zip(once(false).chain(repeat(false).take(4).chain(once(true)).cycle()))
+        .fold(String::new(), |mut acc, (c, b)| {
+            if b { acc.push(' ')};
             acc.push(c); 
             acc
             })
 }
 
 pub fn decode(phrase : &str) -> String {
-    let mapping = get_mapping();
+    let encoder = ArithmeticEncoder{};
 
-    phrase
-        .to_lowercase()
-        .chars()
-        .map(|x| mapping.get(&x).map_or(x, |y| *y) )
-        .filter(|c| c.is_alphanumeric() && c.is_ascii())
-        .fold(String::new(), |mut acc, c| {
-            acc.push(c); 
-            acc
-            })
+    basic_encode(phrase, &encoder).into_iter().collect()
 }
 
-pub fn get_mapping() -> HashMap<char, char> {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    alphabet
-        .chars()
-        .zip(alphabet.chars().rev())
-        .fold(
-            HashMap::new(), 
-            |mut acc, pair| {
-                acc.insert(pair.0, pair.1);
-                acc
-            }
-        )
-            
+struct MapEncoder {
+    encoding : HashMap<char, char>, 
 }
+
+struct ArithmeticEncoder {}
+impl Encoder for ArithmeticEncoder {
+    fn encode(&self, c : &char) -> char {
+        if !c.is_alphabetic() {
+            *c 
+        } 
+        else {
+            let dist_from_a =  (*c as u32) - ('a' as u32);
+            std::char::from_u32('z' as u32 - dist_from_a).unwrap() 
+        }
+    }
+}
+
+// impl MapEncoder {
+//     fn new() -> MapEncoder {
+//         let alphabet = "abcdefghijklmnopqrstuvwxyz";
+//         let map = alphabet
+//                     .chars()
+//                     .zip(alphabet.chars().rev())
+//                     .fold(
+//                         HashMap::new(), 
+//                         |mut acc, pair| {
+//                             acc.insert(pair.0, pair.1);
+//                             acc
+//                         }
+//                     );
+
+//         MapEncoder { encoding : map}
+//     }
+// }
+
+// impl Encoder for MapEncoder {
+//     fn encode(&self, c : &char) -> char {
+//         self.encoding.get(c).map_or(*c, |x| *x)
+//     }
+// }
+
